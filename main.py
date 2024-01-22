c8b63ff29f2e7a7689436b6dc18e0b2ea4f55989
unknown <niraimathi.v04@gmail.com>
2024-01-22 22:45:10 +0530

    Fist commit

---
+++ main.py
@@ -0,0 +1,83 @@
+import os
+
+os.system("cls")
+print(
+    '''
+   ____   _   _   ____    ____    _____   _   _    ____  __   __  
+  / ___| | | | | |  _ \  |  _ \  | ____| | \ | |  / ___| \ \ / /  
+ | |     | | | | | |_) | | |_) | |  _|   |  \| | | |      \ V /   
+ | |___  | |_| | |  _ <  |  _ <  | |___  | |\  | | |___    | |    
+  \____|  \___/  |_| \_\ |_| \_\ |_____| |_| \_|  \____|   |_|    
+
+   ____    ___    _   _  __     __  _____   ____    _____   _____   ____    
+  / ___|  / _ \  | \ | | \ \   / / | ____| |  _ \  |_   _| | ____| |  _ \   
+ | |     | | | | |  \| |  \ \ / /  |  _|   | |_) |   | |   |  _|   | |_) |  
+ | |___  | |_| | | |\  |   \ V /   | |___  |  _ <    | |   | |___  |  _ <   
+  \____|  \___/  |_| \_|    \_/    |_____| |_| \_\   |_|   |_____| |_| \_\  
+
+     _      ____    ____  
+    / \    |  _ \  |  _ \ 
+   / _ \   | |_) | | |_) |
+  / ___ \  |  __/  |  __/ 
+ /_/   \_\ |_|     |_|    
+                          
+                                                                            
+'''
+)
+
+
+os.system("color 8B");
+
+def menu():
+    os.system("cls")
+    print()
+    print()
+    print("\t\t\tMENU")
+    print("\t\t\t----")
+    print("1. Convert")
+    print("2. Add country")
+    print("3. Update")
+    print("4. Credits")
+    print("5. Exit")
+    return int(input("Enter your option: "))
+
+print("\n\n")
+print("Press anykey to continue...", end="")
+input()
+
+while True:
+    option = menu()
+    if option == 1:
+        os.system("cls")
+        print("\t\t\tConvert")
+        print("\t\t\t-------")
+        print("These are the available countries:\n1.France\n2.Cananda\n3.Great Britain\n4.New York\n5.China\n\nFrom Country:\nAmount:")
+        option = menu()
+    elif option == 2:
+        os.system("cls")
+        print("\t\t\tAdd country")
+        print("What are the countries you want to add ?\nCountry Name:\nConversion Value:")
+        option=menu()
+    elif option ==3:
+        os.system("cls")
+        print("\t\t\tUpdate")
+        print("\t\t\t------")
+        print("\nThese are the available countries:\n1.France\n2.Cananda\n3.UK\n4.USA\n5.Europe\n\n\nCountry:\nUpdated value:")
+        option=menu()
+    elif option==4:
+        os.system("cls")
+        print("\t\t\tCredits")
+        print("\t\t\t-------")
+        print("\nWe have done a Currency Converter App\nBY\nSahithya.T.S - ECE(2nd Year)\nNiraimathi.V - ECE(2nd Year)\nYuva Rekha @ Yuva Deeksha.G - Civil(3rd Year)")
+        option=menu()
+    elif option==5:
+        print("Thank you for using this app")
+        input()
+        break
+        os.system("cls")
+    else:
+        print("\nPlease enter a valid option from 1 to 5")
+        input("\nRedirected to Menu")
+
+    
+            
